dad411c74e1d2133a261c667747ce6a314822004
Kirtan Patel <82870418+SerKirtan@users.noreply.github.com>
2023-07-26 18:49:41 +0530

    Add files via upload

---
+++ practical1.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(vector<int>& arr, int length)
+{
+    for(int i = 0 ; i < length-1 ; i++)
+        for(int j = i+1; j < length ; j++)
+            if(arr[i]> arr[j]) swap(arr[i] , arr[j]);
+
+}
+
+void insertionSort(vector<int>& arr , int length){
+    for(int i = 1; i < length ; i++){
+        int temp = arr[i];
+        int j=i-1;
+
+        while(j >= 0){
+            if(arr[j] > temp)
+                arr[j+1] = arr[j];
+            else
+                break;
+            j--;
+        }
+        arr[j+1] = temp;
+    }
+}
+
+void selectionSort(vector<int>& arr , int length){
+    for(int i = 0 ; i < length-1 ; i++){
+        int min = i;
+
+        for(int j = i+1 ; j < length ; j++){
+            if(arr[j] < arr[min]) min = j; 
+        }
+
+        if(min != i)
+            swap(arr[i],arr[min]);
+    }
+}
+
+void print(vector<int>& arr , int length, auto start, auto end){
+    cout << "\n";
+    for(int i = 0 ; i < length ; i++)
+        cout << arr[i] << " ";
+
+    cout << "\n~~Time analysis~~";
+    cout << "\nIn nanoseconds : " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << "ns";
+    cout << "\nIn microseconds : " << chrono::duration_cast<chrono::microseconds>(end - start).count() << "µs";
+    cout << "\nIn milliseconds : " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms";
+    cout << "\n~~Time analysis~~";
+}
+
+int main()
+{
+    vector<int> arr;
+    int size , temp;
+    cout << "\nEnter number of elements you want to sort : ";
+    cin >> size;
+
+    for(int i = 0 ; i < size ; i++){
+        cin >> temp;
+        arr.push_back(temp);
+    }
+
+    int ch = 0;
+    
+    while(ch != 4){
+        cout << "\n1] Bubble sort\n2] Indertion sort\n3] Selection Sort\n4] Exit\nEnter choice :";
+        cin >> ch;
+        auto start = chrono::steady_clock::now();
+        switch(ch){
+            case 1: {
+                bubbleSort(arr, size);
+                auto end = chrono::steady_clock::now();
+                print(arr, size, start, end);
+                break;
+            }
+            case 2: {
+                insertionSort(arr, size);
+                auto end = chrono::steady_clock::now();
+                print(arr, size, start, end);
+                break;
+             }   
+            case 3: {
+                selectionSort(arr, size);
+                auto end = chrono::steady_clock::now();
+                print(arr, size, start, end);
+                break;
+            }
+            case 4:
+                break;
+            default:
+                cout << "Invalid choice. Please enter a valid option.\n";
+                break;
+        }
+    }
+}
---
+++ practical2.cpp
@@ -0,0 +1,127 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void swap(int &n1, int &n2){
+    int temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
+
+void quickSort(vector<int>& array, int const begin, int const end)
+{
+    if(begin >= end) return;
+
+    int pivotIndex = begin + 1;
+    for(int i = begin + 1; i <= end ; i++)
+    {
+        if(array[i] <= array[begin]){
+            swap(array[i], array[pivotIndex]);
+            pivotIndex++;
+        }
+    }
+    swap(array[pivotIndex-1], array[begin]);
+
+    quickSort(array, begin, pivotIndex-1);
+    quickSort(array, pivotIndex+1, end);
+}
+
+//Merges two sorted array
+void mergeArray(vector<int>& array, const int first, const int mid, const int last){
+
+    const int sizeA = mid - first + 1;
+    const int sizeB = last - mid;
+    int indexC = first;
+
+    // create two temp arrays
+    int* arrA = new int[sizeA];
+    int* arrB = new int[sizeB]; 
+
+    for(int i = 0 ; i < sizeA ; i++)
+        arrA[i] = array[first + i];
+    for(int i = 0 ; i < sizeB ; i++)
+        arrB[i] = array[mid + 1 + i];
+
+    int indexA, indexB;
+    indexA = indexB = 0;   
+    //Merge array[arrA .. arrB]
+    while(indexA < sizeA && indexB < sizeB){
+        if(arrA[indexA] <= arrB[indexB] || indexB == sizeB)
+            array[indexC++] = arrA[indexA++];
+        else  
+            array[indexC++] = arrB[indexB++];
+    }
+    //add the remaining elements of arrA and arrB
+    while(indexA < sizeA)
+        array[indexC++] = arrA[indexA++];
+    while(indexB < sizeB)
+        array[indexC++] = arrB[indexB++];
+
+    delete[] arrA;
+    delete[] arrB;
+}
+
+//Divides array into 2 {Recursive}
+void mergeSort(vector<int>& array, const int first, const int last){
+
+    if(first >= last)
+        return;
+
+    int mid = first + (last - first)/2;
+
+    mergeSort(array, first, mid);
+    mergeSort(array, mid + 1, last);
+    mergeArray(array, first, mid, last);
+}
+
+//Prints the array
+void print(vector<int>& arr , int length, auto start, auto end){
+    cout << "\n";
+    for(int i = 0 ; i < length ; i++)
+        cout << arr[i] << " ";
+
+    cout << "\n~~Time analysis~~";
+    cout << "\nIn nanoseconds : " << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << "ns";
+    cout << "\nIn microseconds : " << chrono::duration_cast<chrono::microseconds>(end - start).count() << "µs";
+    cout << "\nIn milliseconds : " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms";
+    cout << "\n~~Time analysis~~";
+}
+
+int main(){
+
+    vector<int> array;
+    int size;
+
+    cout << "Enter the size of array : ";
+    cin >> size;
+
+    cout << "Enter " << size << " elements : ";
+    int temp;
+
+    for(int i = 0 ; i < size ; i++){
+        cin >> temp;
+        array.push_back(temp);
+    }
+
+    int choice = 0;
+
+    cout << "\n1] Merge sort\n2] Quick sort\n3] Exit \nEnter choice :";
+    cin >> choice;
+    auto start = chrono::steady_clock::now();
+
+    switch(choice){
+        case 1: {
+            mergeSort(array, 0, size-1);
+            auto end = chrono::steady_clock::now();
+            print(array, size, start, end);
+        }
+			break;
+        case 2: {
+            quickSort(array, 0, size-1);
+            auto end = chrono::steady_clock::now();
+            print(array, size, start, end);
+        }
+			break;
+        case 3: break;
+        default: cout << "\nPlease enter a valid choice";
+    }
+}
