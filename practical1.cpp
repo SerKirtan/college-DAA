7807ea1787bd5060410382237e255ef962d2b3d0
Kirtan Patel <82870418+SerKirtan@users.noreply.github.com>
2023-08-08 00:52:56 +0530

    Update practical1.cpp

--- practical1.cpp
+++ practical1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void bubbleSort(vector<int>& arr, int length)
 {
     for(int i = 0 ; i < length-1 ; i++)
-        for(int j = i+1; j < length ; j++)
+        for(int j = 1; j < length-i ; j++)
             if(arr[j-1]> arr[j]) swap(arr[j-1] , arr[j]);
 
 }
